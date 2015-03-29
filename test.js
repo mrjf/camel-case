bfb4419c1447466725f3da17a7188db011a55059
Blake Embrey <hello@blakeembrey.com>
2015-03-29 16:15:44 -0300

    update to standard style

--- test.js
+++ test.js
@@ -1,37 +1,37 @@
 /* global describe, it */
-var assert    = require('assert');
-var camelCase = require('./');
+var assert = require('assert')
+var camelCase = require('./')
 
 describe('camel case', function () {
   it('should lower case a single word', function () {
-    assert.equal(camelCase('test'), 'test');
-    assert.equal(camelCase('TEST'), 'test');
-  });
+    assert.equal(camelCase('test'), 'test')
+    assert.equal(camelCase('TEST'), 'test')
+  })
 
   it('should camel case regular sentence cased strings', function () {
-    assert.equal(camelCase('test string'), 'testString');
-    assert.equal(camelCase('Test String'), 'testString');
-  });
+    assert.equal(camelCase('test string'), 'testString')
+    assert.equal(camelCase('Test String'), 'testString')
+  })
 
   it('should camel case non-alphanumeric separators', function () {
-    assert.equal(camelCase('dot.case'), 'dotCase');
-    assert.equal(camelCase('path/case'), 'pathCase');
-  });
+    assert.equal(camelCase('dot.case'), 'dotCase')
+    assert.equal(camelCase('path/case'), 'pathCase')
+  })
 
   it('should underscore periods inside numbers', function () {
-    assert.equal(camelCase('version 1.2.10'), 'version1_2_10');
-    assert.equal(camelCase('version 1.21.0'), 'version1_21_0');
-  });
+    assert.equal(camelCase('version 1.2.10'), 'version1_2_10')
+    assert.equal(camelCase('version 1.21.0'), 'version1_21_0')
+  })
 
   it('should camel case pascal cased strings', function () {
-    assert.equal(camelCase('TestString'), 'testString');
-  });
+    assert.equal(camelCase('TestString'), 'testString')
+  })
 
   it('should camel case non-latin strings', function () {
-    assert.equal(camelCase('simple éxample'), 'simpleÉxample');
-  });
+    assert.equal(camelCase('simple éxample'), 'simpleÉxample')
+  })
 
   it('should support locale', function () {
-    assert.equal(camelCase('STRING 1.2', 'tr'), 'strıng1_2');
-  });
-});
+    assert.equal(camelCase('STRING 1.2', 'tr'), 'strıng1_2')
+  })
+})
